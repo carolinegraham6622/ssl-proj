7085ea9c7a101988e8e986a1bf210f5ac74f419d
Graham <graham26@students.rowan.edu>
2022-06-21 14:37:29 -0400

    last 30 days

--- web/webpage.py
+++ web/webpage.py
@@ -6,12 +6,31 @@ conn = pymysql.connect(database='ssl_check', host='db', user='root', password='1
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    #return render_template('./indextest.html')
+def home():
     cur = conn.cursor()
+    #pull from the last 30 days
+    cur.execute("select * from cert where start_time > NOW() - INTERVAL 30 DAY;")
+    results = cur.fetchall()
+    #conn.close()
+    return render_template('./home.html', results=results)
+
+@app.route("/lastYear/")
+def lastYear():
+    cur = conn.cursor()
+    cur.execute("select * from cert where start_time > NOW() - INTERVAL 365 DAY;")
+    results = cur.fetchall()
+    #conn.close()
+    return render_template('./home.html', results=results)
+
+@app.route("/showAll/")
+def showAll():
+    cur = conn.cursor()
+    #pull all rows
     cur.execute("select * from cert")
     results = cur.fetchall()
     #conn.close()
-    return render_template('./index.html', results=results)
+    return render_template('./home.html', results=results)
+
+
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
